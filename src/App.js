1fa137288c07acc2672912ce45c392062e29122d
hermon1 <hermon.tesfay@gmail.com>
2023-11-12 11:52:08 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
-
+import "./App.css";
+import "./css/style.css";
+// import "./sass/style.css";
 import NavigationLayout from "./components/Common/Navigation";
 import { AuthProvider } from "./context/AuthContext";
 import Footer from "./components/Common/Footer";
-
 
 function App() {
   return (
